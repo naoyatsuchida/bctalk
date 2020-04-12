5495c17717a8933a1e394dfca1634b57da479ec2
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-12 21:38:19 +0900

    ここまでコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+    delete 'destroy_user_session', to: 'users/sessions#destroy'
+
+  end
   root  'posts#index'
 end
