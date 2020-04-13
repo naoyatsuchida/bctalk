130083235d741551061f2b9a196e5d8ae4b5b41c
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-13 17:13:10 +0900

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
 
-         belongs_to :industry #ユーザーは一つの業界に所属している
-         belongs_to :occupation #ユーザーは一つの業界に所属している
-         has_many :posts
-         #バリデーションは後で
+          belongs_to :industry #ユーザーは一つの業界に所属している
+          belongs_to :occupation #ユーザーは一つの業界に所属している
+          has_many :posts
+       
+          validates :email,:nickname,:industry_id,:occupation_id ,presence: true
+          VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+          validates :email, uniqueness: true,format: { with: VALID_EMAIL_REGEX }
+          validates :password,:password_confirmation, length:{minimum:6}         
 end
