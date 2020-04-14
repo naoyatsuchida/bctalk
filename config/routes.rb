c265346c7a8cf31ff08db82e08f0f196e578b805
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-14 13:11:46 +0900

    haml 導入して

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
     get 'user/searchin', to: 'users/registrations#searchin'
     get 'user/searchoc', to: 'users/registrations#searchoc'
   end
+  constraints ->  request { request.session[:user_id].present? } do
+
+    root  "posts#home"
+  end
+
   root  'posts#index'
+  resources :posts
 end
