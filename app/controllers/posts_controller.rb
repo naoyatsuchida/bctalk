3f6690fb0528f9cacbf84313c1fdb278c98a999c
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-17 14:30:00 +0900

    記事投稿ページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,15 @@
 class PostsController < ApplicationController
-  def home 
-  end
-
+before_action :set_post, only:[:new]
   def index
     
+  end
+
+  def new 
+    
+  end
+
+  private
+  def set_post
+    @post = Post.new
   end
 end
