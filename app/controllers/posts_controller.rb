62b93733a2dc6b89d7998a007f70714ca077ac16
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-17 17:59:52 +0900

    editフォーム　ストロングまだ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,20 @@ before_action :set_post, only:[:new]
     
   end
 
+  def create 
+    post = Post.new(strong_param)
+    if post.save
+      redirect_to root_path
+    else 
+      render :new 
+    end
+  end
+
   private
+  def strong_param
+    params.require(:post).permit(:title,:content).merge(user_id: current_user.id)
+
+  end
   def set_post
     @post = Post.new
   end
