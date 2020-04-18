115ad2fca26e1d2e33c864bd97f57e267edb7dba
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-18 18:18:39 +0900

    編集ページ崩れた最悪！

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
           has_many :groups,through: :group_users
           has_many :group_users
           has_many :messages
+          # mount_uploader :images, ImageUploader
           
           validates :email,:nickname,:industry_id,:occupation_id ,presence: true
           VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
